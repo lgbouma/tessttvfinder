7b7c7d061d1f2af14872a4d617f83dc4c2f0fce8
lgbouma <lgbouma@mit.edu>
2018-10-19 13:58:01 -0400

    add retrieve_measured_times_pickle

--- src/retrieve_measured_times.py
+++ src/retrieve_measured_times.py
@@ -16,10 +16,14 @@ from measure_transit_times_from_lightcurve import get_transit_times, \
         get_limb_darkening_initial_guesses, get_a_over_Rstar_guess
 
 from glob import glob
+from numpy import array as nparr
+
+import os, argparse, pickle, h5py
 
 np.random.seed(42)
 
-def retrieve_measured_times(ticid):
+def retrieve_measured_times_mcmc(ticid):
+    # NOTE: outdated. phase out soon!
 
     sampledir = '/Users/luke/local/emcee_chains/'
     fpattern = '{:s}_mandelagol_fit_samples_6d_t???.h5'.format(str(ticid))
@@ -139,8 +143,6 @@ def retrieve_measured_times(ticid):
             print('transit {:d} failed, continue'.format(transit_ix))
             continue
 
-    from numpy import array as nparr
-
     t0, t0_merr, t0_perr, t0_bigerr = (
         nparr(t0_list),nparr(t0_merrs),nparr(t0_perrs),nparr(t0_bigerrs)
     )
@@ -157,5 +159,77 @@ def retrieve_measured_times(ticid):
     print('saved to {:s}'.format(outdir+outname))
 
 
+def retrieve_measured_times_pickle(
+    ticid, pickledir='../results/tess_lightcurve_fit_parameters/',
+    sampledir='/home/luke/local/emcee_chains/'
+):
+
+    # empirical errors -> believable error bars!
+    fpattern = (
+        '{:s}_mandelagol_fit_empiricalerrs_t???.pickle'.
+        format(str(ticid))
+    )
+    fnames = np.sort(glob(sampledir+fpattern))
+
+    samplepattern = (
+        '{:s}_mandelagol_fit_samples_4d_t???_empiricalerrs.h5'.
+        format(str(ticid))
+    )
+    samplenames = np.sort(glob(sampledir+samplepattern))
+
+    t0_list, t0_merrs, t0_perrs, t0_bigerrs, samplepaths, picklepaths = (
+        [],[],[],[],[],[]
+    )
+
+    for fname, samplename in zip(fnames, samplenames):
+
+        d = pickle.load(fname)
+
+        fitparams = d['fitinfo']['finalparams']
+        fiterrs = d['fitinfo']['finalparamerrs']
+
+        t0_list.append(fitparams['t0'])
+        t0_merrs.append(fiterrs['std_merrs']['t0'])
+        t0_perrs.append(fiterrs['std_perrs']['t0'])
+        t0_bigerrs.append(max(
+            fiterrs['std_merrs']['t0'],fiterrs['std_perrs']['t0']))
+        samplepaths.append(samplename)
+        picklepaths.append(fname)
+
+        except Exception as e:
+            print(e)
+            print('transit {:d} failed, continue'.format(transit_ix))
+            continue
+
+    t0, t0_merr, t0_perr, t0_bigerr = (
+        nparr(t0_list),nparr(t0_merrs),nparr(t0_perrs),nparr(t0_bigerrs)
+    )
+
+    df = pd.DataFrame({
+        't0_BTJD':t0, 't0_merr':t0_merr, 't0_perr':t0_perr,
+        't0_bigerr':t0_bigerr, 'BJD_TDB':t0+2457000, 'samplepath':samplepaths,
+        'picklepath':picklepaths
+    })
+    outdir = '../data/'
+    outname = (
+        str(ticid)+'_measured_TESS_times_{:d}_transits.csv'.format(transit_ix)
+    )
+    df.to_csv(outdir+outname, index=False)
+    print('saved to {:s}'.format(outdir+outname))
+
+
 if __name__ == "__main__":
-    retrieve_measured_times(231663901)
+
+    parser = argparse.ArgumentParser(
+        description=('Given a lightcurve with transits (e.g., alerted '
+                     'from TESS Science Office), measure the times that they '
+                     'fall at by fitting models.'))
+
+    parser.add_argument('--ticid', type=int, default=None,
+        help=('integer TIC ID for object. Pickle paramfile assumed to be at '
+              '../results/tess_lightcurve_fit_parameters/'
+             ))
+
+    args = parser.parse_args()
+
+    retrieve_measured_times_pickle(args.ticid)
