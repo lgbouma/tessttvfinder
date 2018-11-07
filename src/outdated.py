90e2e005eff858e14ec8464bd9cd65e54392241b
lgbouma <lgbouma@mit.edu>
2018-11-07 16:59:52 -0500

    retrieving times from MCMC: cleanup
    `retrieve_measured_times_mcmc` was an outdated functon.
    `retrieve_measured_times_pickle` is what's currently used.

--- src/outdated.py
+++ src/outdated.py
@@ -1,3 +1,141 @@
+def retrieve_measured_times_mcmc(ticid):
+    # NOTE: outdated. phase out soon!
+
+    sampledir = '/Users/luke/local/emcee_chains/'
+    fpattern = '{:s}_mandelagol_fit_samples_6d_t???.h5'.format(str(ticid))
+    fnames = glob(sampledir+fpattern)
+
+    ##########################################
+    # detrending parameters. mingap: minimum gap to determine time group size.
+    # smooth_window_day: window for median filtering.
+    mingap = 240./60./24.
+    smooth_window_day = 2.
+    cadence_min = 2
+    make_diagnostic_plots = True
+
+    cadence_day = cadence_min / 60. / 24.
+    windowsize = int(smooth_window_day/cadence_day)
+    if windowsize % 2 == 0:
+        windowsize += 1
+
+    # paths for reading and writing plots
+    lcdir = '../data/tess_lightcurves/'
+    lcname = 'tess2018206045859-s0001-{:s}-111-s_llc.fits.gz'.format(
+                str(ticid).zfill(16))
+    lcfile = lcdir + lcname
+    ##########################################
+
+    time, flux, err_flux = at.get_time_flux_errs_from_Ames_lightcurve(
+                                lcfile, 'PDCSAP')
+
+    # get time groups, and median filter each one
+    ngroups, groups = lcmath.find_lc_timegroups(time, mingap=mingap)
+
+    tg_smooth_flux = []
+    for group in groups:
+        tg_flux = flux[group]
+        tg_smooth_flux.append(
+            smooth_magseries_ndimage_medfilt(tg_flux, windowsize)
+        )
+
+    smooth_flux = np.concatenate(tg_smooth_flux)
+    whitened_flux = flux/smooth_flux
+
+    # run bls to get initial parameters.
+    endp = 1.05*(np.nanmax(time) - np.nanmin(time))/2
+    blsdict = kbls.bls_parallel_pfind(time, flux, err_flux, magsarefluxes=True,
+                                      startp=0.1, endp=endp,
+                                      maxtransitduration=0.3, nworkers=8,
+                                      sigclip=10.)
+    fitd = kbls.bls_stats_singleperiod(time, flux, err_flux,
+                                       blsdict['bestperiod'],
+                                       magsarefluxes=True, sigclip=10.,
+                                       perioddeltapercent=5)
+
+    # fit Mandel & Agol model to each single transit, +/- 5 transit durations
+    tmids, t_starts, t_ends = get_transit_times(fitd, time, 5)
+
+    t0_list, t0_merrs, t0_perrs, t0_bigerrs, samplepaths = [],[],[],[],[]
+
+    for transit_ix, t_start, t_end in list(
+        zip(range(len(t_starts)), t_starts, t_ends)
+    ):
+
+        try:
+            sel = (time < t_end) & (time > t_start)
+            sel_time = time[sel]
+            sel_whitened_flux = whitened_flux[sel]
+            sel_err_flux = err_flux[sel]
+
+            u_linear, u_quad = get_limb_darkening_initial_guesses(lcfile)
+            a_guess = get_a_over_Rstar_guess(lcfile, fitd['period'])
+
+            rp = np.sqrt(fitd['transitdepth'])
+
+            initfitparams = {'t0':t_start + (t_end-t_start)/2., 'rp':rp,
+                             'sma':a_guess, 'incl':85, 'u':[u_linear,u_quad] }
+
+            fixedparams = {'ecc':0., 'omega':90., 'limb_dark':'quadratic',
+                           'period':fitd['period'] }
+
+            priorbounds = {'rp':(rp-0.01, rp+0.01),
+                           'u_linear':(u_linear-1, u_linear+1),
+                           'u_quad':(u_quad-1, u_quad+1),
+                           't0':(np.min(sel_time), np.max(sel_time)),
+                           'sma':(0.7*a_guess,1.3*a_guess), 'incl':(75,90) }
+
+            t_num = str(transit_ix).zfill(3)
+            sample_plotname = (
+                str(ticid)+'_mandelagol_fit_samples_6d_t{:s}.h5'.format(t_num)
+            )
+
+            chain_savdir = '/Users/luke/local/emcee_chains/'
+            samplesavpath = chain_savdir + sample_plotname
+
+            print('beginning {:s}'.format(samplesavpath))
+
+            mandelagolfit = lcfit.mandelagol_fit_magseries(
+                            sel_time, sel_whitened_flux, sel_err_flux,
+                            initfitparams, priorbounds, fixedparams,
+                            trueparams=None, magsarefluxes=True,
+                            sigclip=10., plotfit=None,
+                            plotcorner=None,
+                            samplesavpath=samplesavpath, nworkers=8,
+                            n_mcmc_steps=42, eps=1e-1, n_walkers=500,
+                            skipsampling=True, overwriteexistingsamples=False)
+
+            fitparams = mandelagolfit['fitinfo']['finalparams']
+            fiterrs = mandelagolfit['fitinfo']['finalparamerrs']
+
+            t0_list.append(fitparams['t0'])
+            t0_merrs.append(fiterrs['std_merrs']['t0'])
+            t0_perrs.append(fiterrs['std_perrs']['t0'])
+            t0_bigerrs.append(max(
+                fiterrs['std_merrs']['t0'],fiterrs['std_perrs']['t0']))
+            samplepaths.append(samplesavpath)
+
+        except Exception as e:
+            print(e)
+            print('transit {:d} failed, continue'.format(transit_ix))
+            continue
+
+    t0, t0_merr, t0_perr, t0_bigerr = (
+        nparr(t0_list),nparr(t0_merrs),nparr(t0_perrs),nparr(t0_bigerrs)
+    )
+
+    df = pd.DataFrame({
+        't0_BTJD':t0, 't0_merr':t0_merr, 't0_perr':t0_perr,
+        't0_bigerr':t0_bigerr, 'BJD_TDB':t0+2457000, 'samplepath':samplepaths
+    })
+    outdir = '../data/'
+    outname = (
+        str(ticid)+'_measured_TESS_times_{:d}_transits.csv'.format(transit_ix)
+    )
+    df.to_csv(outdir+outname, index=False)
+    print('saved to {:s}'.format(outdir+outname))
+
+
+
 def scatter_plot_parameter_vs_epoch_etd(df, yparam, datafile, init_period,
                                         init_t0, overwrite=False,
                                         savname=None):
