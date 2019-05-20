050576533a817624c9f72bbb4cf6c44ac6509ed0
lgbouma <bouma.luke@gmail.com>
2019-05-20 14:34:08 -0400

    update for wasp-31

--- drivers/measure_sector_4_thru_9_times.sh
+++ drivers/measure_sector_4_thru_9_times.sh
@@ -13,21 +13,8 @@ n_phase_mcmc=1000 #  at 1000, 15 minutes per phase transit. (30 total),
 n_workers=16 # number of workers on ast!
 n_transit_durations=2
 
-ticid=22529346 # WASP-121b
-lcdir='/home/luke/local/tess_mast_lightcurves/tic_'${ticid}'/'
 
-# python -u measure_transit_times_from_lightcurve.py \
-#   --ticid $ticid --n_mcmc_steps $n_mcmc \
-#   --n_phase_mcmc_steps $n_phase_mcmc \
-#   --no-getspocparams --read_literature_params \
-#   --overwritesamples --no-mcmcprogressbar \
-#   --nworkers $n_workers --chain_savdir $chain_savdir --lcdir $lcdir \
-#   --no-verify-times \
-#   --n_transit_durations $n_transit_durations
-# 
-# exit 1 # FIXME
-
-ticid=36352297 # CoRoT-1b
+ticid=437248515 # WASP-31b
 lcdir='/home/luke/local/tess_mast_lightcurves/tic_'${ticid}'/'
 python -u measure_transit_times_from_lightcurve.py \
   --ticid $ticid --n_mcmc_steps $n_mcmc \
@@ -39,6 +26,32 @@ python -u measure_transit_times_from_lightcurve.py \
   --n_transit_durations $n_transit_durations
 
 exit 1
+
+# ticid=22529346 # WASP-121b
+# lcdir='/home/luke/local/tess_mast_lightcurves/tic_'${ticid}'/'
+# python -u measure_transit_times_from_lightcurve.py \
+#   --ticid $ticid --n_mcmc_steps $n_mcmc \
+#   --n_phase_mcmc_steps $n_phase_mcmc \
+#   --no-getspocparams --read_literature_params \
+#   --overwritesamples --no-mcmcprogressbar \
+#   --nworkers $n_workers --chain_savdir $chain_savdir --lcdir $lcdir \
+#   --no-verify-times \
+#   --n_transit_durations $n_transit_durations
+# 
+# exit 1
+
+# ticid=36352297 # CoRoT-1b
+# lcdir='/home/luke/local/tess_mast_lightcurves/tic_'${ticid}'/'
+# python -u measure_transit_times_from_lightcurve.py \
+#   --ticid $ticid --n_mcmc_steps $n_mcmc \
+#   --n_phase_mcmc_steps $n_phase_mcmc \
+#   --no-getspocparams --read_literature_params \
+#   --overwritesamples --no-mcmcprogressbar \
+#   --nworkers $n_workers --chain_savdir $chain_savdir --lcdir $lcdir \
+#   --no-verify-times \
+#   --n_transit_durations $n_transit_durations
+# 
+# exit 1
 
 # ticid=35516889 # WASP-19b
 # lcdir='/home/luke/local/tess_mast_lightcurves/tic_'${ticid}'/'
